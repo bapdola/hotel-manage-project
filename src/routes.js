2d1e83bd8905cb1cef907b29d34f4c95fb419abb
DinhThienThoai <92067875+vmaxmartis@users.noreply.github.com>
2022-03-30 10:55:05 +0700

    UserManagerment page

--- src/routes.js
+++ src/routes.js
@@ -9,10 +9,18 @@ const Bookings = React.lazy(() =>
 const editBooking = React.lazy(() =>
   import("./views/dashboard/bookings/editBooking")
 );
+const Users = React.lazy(() =>
+  import("./views/dashboard/users/ManagerUser")
+);
+const Rooms = React.lazy(() =>
+  import("./views/dashboard/rooms/rooms")
+);
+const AddRoom = React.lazy(() => import("./views/dashboard/rooms/addRoom"))
 const routes = [
   { path: "/", exact: true, name: "Home" },
-  { path: "/dashboard", name: "Rooms", element: Dashboard },
-  { path: "/users", name: "Users", element: Dashboard },
+  { path: "/rooms", name: "Rooms", element: Rooms },
+  { path: '/Rooms/addroom', name: 'Rooms', element: AddRoom },
+  { path: "/user", name: "Users", element: Users },
   { path: "/bookings", name: "Bookings", element: Bookings },
   { path: "/bookings/edit", name: "Bookings", element: editBooking },
 ];
