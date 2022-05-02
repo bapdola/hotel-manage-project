c3b9573a09748183feb714f27e7dcc1211f60377
Truong Trong Thien <thienvagabond113@gmail.com>
2022-05-02 17:00:52 +0700

    Final project

--- src/views/dashboard/users/popupadd.js
+++ src/views/dashboard/users/popupadd.js
@@ -68,11 +68,7 @@ const PopupAdd = () => {
 
   return (
     <>
-      <CButton
-        variant="outline"
-        color="success"
-        onClick={() => setVisibleLg(!visibleLg)}
-      >
+      <CButton color="success" onClick={() => setVisibleLg(!visibleLg)}>
         <VscAdd size={15} /> Add
       </CButton>
       <CModal size="lg" visible={visibleLg} onClose={handleReset}>
